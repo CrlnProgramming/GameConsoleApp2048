2773f56bb0ce405c4dca316babf28d64e548a07a
ivan Ivanov <pp1yshka@mail.ru>
2022-05-01 22:31:13 +0300

    Add project files.

---
+++ ConsoleApp2048/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+using Game2048;
+
+namespace ConsoleApp2048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            //Todo: program.Download();
+            program.Start();
+
+        }
+        
+        void Start()
+        {
+            Model model= new Model(4);
+            model.Start();
+            while (true)
+            {
+                Show(model);
+                switch (Console.ReadKey(false).Key)
+                {
+                    case ConsoleKey.LeftArrow: model.Left();break;
+                    case ConsoleKey.RightArrow: model.Right(); break;
+                    case ConsoleKey.UpArrow: model.Up(); break;
+                    case ConsoleKey.DownArrow: model.Down(); break;
+                    case ConsoleKey.Escape: return;
+                    case ConsoleKey.F4: model.Start();break;
+                }
+            }
+        }
+
+        void Show(Model model)
+        {
+            for (int y = 0; y < model.size;y++)
+                for (int x = 0; x < model.size; x++)
+                {
+                    Console.SetCursorPosition(x * 5 + 5, y * 2 + 2);
+                    int number = model.GetMap(x, y);
+                    Console.Write(number == 0 ? ". " : number.ToString() + " ");
+                }
+            Console.WriteLine();
+            if (model.IsGameOver())
+                Console.WriteLine("Game Over");
+            else 
+                Console.WriteLine("Still play");
+        }
+
+        void Download()
+        {
+            var count_poces = 5;
+            int steps = 100 / count_poces + 1;
+            Console.WriteLine("Download");
+            Console.WriteLine("[{0}]", new String(' ', steps));
+
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            for (int process = 0; process < steps; process++)
+            {
+                Console.SetCursorPosition(process + 1, Console.CursorTop);
+                Console.Write("|");
+
+                Console.SetCursorPosition(steps + 3, Console.CursorTop);
+                Console.Write("{0}%", process * count_poces);
+
+                Random r = new Random();
+                Thread.Sleep(r.Next(40, 600));
+            }
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hi!Its my Console 2048 Game");
+        }
+    }
+}
---
+++ Game2048/Model.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game2048
+{
+    public class Model
+    {
+        Map map;
+        static Random random = new Random();
+        bool isGameOver;
+        bool Moved;
+        public int size 
+        {
+            get 
+            { 
+                return map.size; 
+            }
+        }
+        public Model(int size)
+        {
+            map = new Map(size);
+        }
+
+        public void Start()
+        {
+            isGameOver = false;
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    map.Set(x, y, 0);
+            AddRandomNumber();
+            AddRandomNumber();
+        }
+
+        private void AddRandomNumber()
+        {
+            if (isGameOver) return;
+            for (int j = 0; j < 100; j++)
+            {
+                int x = random.Next(0, map.size);
+                int y = random.Next(0, map.size);
+                if (map.Get(x, y) == 0)
+                {
+                    map.Set(x, y, random.Next(1, 3) * 2);
+                    return;
+                }
+            }
+        }
+        void Lift (int x,int y,int sx,int sy)
+        {
+            if (map.Get(x, y) > 0)
+                while (map.Get(x + sx, y + sy) == 0) 
+                {
+                    map.Set(x + sx, y + sy, map.Get(x, y));
+                    map.Set(x, y, 0);
+                    x += sx;
+                    y += sy;
+                    Moved = true;
+                }
+            
+        }
+        void Join(int x, int y, int sx, int sy) 
+        {
+            if(map.Get(x,y)>0)
+                if (map.Get(x + sx, y + sy) == map.Get(x, y))
+                {
+                    map.Set(x + sx, y + sy, map.Get(x, y) * 2);
+                    while (map.Get(x - sx, y - sy) > 0)
+                    {
+                        map.Set(x, y, map.Get(x - sx, y - sy));
+                        x -= sx;
+                        y -= sy;
+                        Moved = true;
+                    }
+                    map.Set(x, y, 0);
+                }
+        }
+
+        public void Left()
+        {
+            Moved = false;
+            for (int y = 0; y < map.size; y++)
+            {
+                for (int x = 0; x < map.size; x++)
+                    Lift(x, y, -1, 0);
+                for (int x = 0; x < map.size; x++)
+                    Join(x, y, -1, 0);
+            }
+            if (Moved)AddRandomNumber();
+        }
+
+        public void Right()
+        {
+            Moved = false;
+            for (int y = 0; y < map.size; y++)
+            {
+                for (int x = map.size - 2; x >= 0; x--)
+                    Lift(x, y, +1, 0);
+                for (int x = map.size - 2; x >= 0; x--)
+                    Join(x, y, +1, 0);
+            }
+            if (Moved) AddRandomNumber();
+        }
+
+        public void Up()
+        {
+            Moved = false;
+            for (int x = 0; x < map.size; x++)
+            {
+                for (int y = 1; y < map.size - 1; y++)
+                    Lift(x, y, 0, -1);
+                for (int y = 1; y < map.size - 1; y++)
+                    Join(x, y, 0, -1);
+            }
+            if (Moved) AddRandomNumber();
+        }
+
+        public void Down()
+        {
+            Moved = false;
+            for (int x = 0; x < map.size; x++)
+            {
+                for (int y = map.size-2; y >=0; y--)
+                    Lift(x, y, 0, +1);
+                for (int y = map.size-2; y >=0; y--)
+                    Join(x, y, 0, +1);
+            }
+            if (Moved) AddRandomNumber();
+        }
+
+        public int GetMap(int x, int y)
+        {
+            return map.Get(x, y);
+        }
+
+        public bool IsGameOver()
+        {
+            if (isGameOver)
+                return isGameOver;
+
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    if (map.Get(x, y) == 0)
+                        return false;
+
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    if (map.Get(x, y) == map.Get(x + 1, y) || map.Get(x, y) == map.Get(x, y + 1))
+                        return false;
+            isGameOver = true;
+            return isGameOver;
+        }
+    }
+}
